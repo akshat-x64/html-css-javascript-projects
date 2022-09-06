165a8bae0ab398b33acb04c5e9df146c0a0dc701
Akshat Dwivedi <93469027+akshat-x64@users.noreply.github.com>
2022-09-06 15:06:59 +0530

    Add files via upload

---
+++ 01_temperature convert/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const value1 = document.querySelector(".value-1");
+const button1 = document.querySelector(".button");
+const value = document.querySelector(".unit1");
+const input = document.querySelector(".value");
+button1.addEventListener("click", function () {
+  let opt = value.value;
+  const data = value1.value;
+
+  if (opt === "Fahrenheit") {
+    const result = (data * 9) / 5 + 32;
+    input.textContent = Math.trunc(result);
+    value1.value = "";
+  } else {
+    const result = ((data - 32) * 5) / 9;
+    input.textContent = Math.trunc(result);
+    value1.value = "";
+  }
+});
