f1231281bfc803b8f5dda31554b02b25946c257e
Akshat Dwivedi <93469027+akshat-x64@users.noreply.github.com>
2022-09-06 18:16:50 +0530

    Update index.js

--- 01_temperature convert/index.js
+++ 01_temperature convert/index.js
@@ -8,13 +8,15 @@ button1.addEventListener("click", function () {
   let opt = value.value;
   const data = value1.value;
 
-  if (opt === "Fahrenheit") {
-    const result = (data * 9) / 5 + 32;
-    input.textContent = Math.trunc(result);
-    value1.value = "";
-  } else {
-    const result = ((data - 32) * 5) / 9;
-    input.textContent = Math.trunc(result);
-    value1.value = "";
+  if (data) {
+    if (opt === "Fahrenheit") {
+      const result = (data * 9) / 5 + 32;
+      input.textContent = Math.trunc(result);
+      value1.value = "";
+    } else {
+      const result = ((data - 32) * 5) / 9;
+      input.textContent = Math.trunc(result);
+      value1.value = "";
+    }
   }
 });
